94bd504eeef8588e3dd2f2a836e8d49807dacc59
Miko <mikogurrobat0@gmail.com>
2023-11-15 21:11:40 +0800

    update flex to grid

--- src/firebase/Base.tsx
+++ src/firebase/Base.tsx
@@ -14,6 +14,6 @@ const firebaseConfig = {
    measurementId: ENV.VITE_MEASUREMENT_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
