65b7c6b226fd5e30db09c14dbb8b2506c683b49d
Miko <mikogurrobat0@gmail.com>
2024-01-26 16:17:09 +0800

    remove unused declare

--- src/components/layout/main-layout/Layout.tsx
+++ src/components/layout/main-layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { messaging } from "../../../firebase/Base";
 import { getToken } from "firebase/messaging";
 import { Outlet } from "react-router-dom";
