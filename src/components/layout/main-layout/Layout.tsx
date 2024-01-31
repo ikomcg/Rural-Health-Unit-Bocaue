9d3ab16275f3eb8b59b6f91c2cf42fbd6bf97910
Miko <mikogurrobat0@gmail.com>
2024-01-31 11:29:04 +0800

    update permission notification

--- src/components/layout/main-layout/Layout.tsx
+++ src/components/layout/main-layout/Layout.tsx
@@ -2,33 +2,39 @@ import { useEffect } from "react";
 import { messaging } from "../../../firebase/Base";
 import { getToken } from "firebase/messaging";
 import { Outlet } from "react-router-dom";
-import Swal from "sweetalert2";
-
 const Layout = () => {
-   const GetFCM = async () => {
-      Notification.requestPermission().then((res) => {
-         if (res === "granted") {
-            return getToken(messaging, {
-               vapidKey:
-                  "BBzsG61OFxBiCuH6DlEOTBSCefgrJ1jKhc1MhQDGzbC4PXbPhIbkrymKqUAxzTsT4urUQ6en5W66j1KpcSJic7E",
-            })
-               .then((currentToken) => {
-                  Swal.fire({
-                     icon: "info",
-                     text: currentToken,
-                  });
-                  console.log("Token", currentToken);
-               })
-               .catch((err) => {
-                  console.log("firebase:", err);
-               });
-         }
-      });
+   const GetToken = async () => {
+      const vapidKey =
+         "BBzsG61OFxBiCuH6DlEOTBSCefgrJ1jKhc1MhQDGzbC4PXbPhIbkrymKqUAxzTsT4urUQ6en5W66j1KpcSJic7E";
+
+      return await getToken(messaging, {
+         vapidKey,
+      })
+         .then((currentToken) => {
+            // eslint-disable-next-line no-console
+            console.log("Token", currentToken);
+         })
+         .catch((err) => err);
+   };
+
+   const CheckNotificationPermission = async () => {
+      if (!("Notification" in window)) return;
+
+      if (Notification.permission === "granted") {
+         GetToken();
+      } else if (Notification.permission !== "denied") {
+         Notification.requestPermission().then((permission) => {
+            if (permission === "granted") {
+               GetToken();
+            }
+         });
+      }
    };
 
    useEffect(() => {
-      GetFCM();
+      CheckNotificationPermission();
    }, []);
+
    return <Outlet />;
 };
 
